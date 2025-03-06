a1a445c68880b4fee2f4df77cd4e56d1d98499c7
kevinvalen12 <kevinvalencia894@gmail.com>
2025-03-06 12:01:35 -0500

    migrate

--- 03-laravel/petsapp/database/migrations/0001_01_01_000000_create_users_table.php
+++ 03-laravel/petsapp/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->bigInteger('document')->unique();
+            $table->string('fullname');
+            $table->string('gender');
+            $table->date('birthdate');
+            $table->string('photo')->default('no-photo');
+            $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('custumer');
             $table->rememberToken();
             $table->timestamps();
+            
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
