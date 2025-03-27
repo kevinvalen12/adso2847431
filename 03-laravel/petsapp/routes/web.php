3c4757e0add079a6ef7fd9c9e07de71de750d158
kevinvalen12 <valenciakevin894@gmail.com>
2025-03-27 11:36:20 -0500

    agregar nuevas rutas para mostrar usuarios y mascotas, y crear vista Blade para la presentación de mascotas

--- 03-laravel/petsapp/routes/web.php
+++ 03-laravel/petsapp/routes/web.php
@@ -1,20 +1,67 @@
 <?php
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+use App\Models\User as User;
+use App\Models\Pet as Pet;
+use illuminate\Support\Carbon;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-// lista de los usuarios
 
+//list all users (Factory challenge
 Route::get('show/users', function () {
-    $users = App\Models\User::all();
-    //dd($users->toArray());
-    
+    $users = User::all();
+   //d($users->toArray());
     return view('users-factory')->with('users', $users);
 });
 
+Route::get('hello', function(){
+    return "<h1>Hello Laravel</h1>";
+})->name('hello');
+
+Route::get('show/pets', function(){
+    $pets = Pet::all();
+    //var_dump($pets->toArray());
+    dd($pets->toArray());
+});
+
+Route::get('user/list', function(){
+    $users = User::take(20)->get();
+    
+    $code = "<table style='border-collapse: collapse; margin: 2rem auto; font-family: Arial'>
+    <tr>
+        <th style='background: gray; color: white; padding: 0.4rem'>Id</th>
+        <th style='background: gray; color: white; padding: 0.4rem'>Fullname</th>
+        <th style='background: gray; color: white; padding: 0.4rem'>Age</th>
+        <th style='background: gray; color: white; padding: 0.4rem'>Created At</th>
+    </tr>";
+
+    foreach($users as $user){
+        $code .= ($user->id%2 == 0) ? "<tr style='background: #ccc'>":"<tr>";
+        $code .= "<td style='border: 1px solid gray; padding: 0.4rem'>".$user->id."</td>";
+        $code .= "<td style='border: 1px solid gray; padding: 0.4rem'>".$user->fullname."</td>";
+        $code .= "<td style='border: 1px solid gray; padding: 0.4rem'>".Carbon::parse($user->birthdate)->age." years old</td>";
+        $code .= "<td style='border: 1px solid gray; padding: 0.4rem'>".$user->created_at->diffForHumans()."</td>";
+        $code .= "</tr>";
+    }
+    return $code .= "</table>";
+});
+
+Route::get('view/blade', function(){
+    $tittle = "Example with Blade";
+
+    // tambien se puede poner en un agreglo seria whereIn['dog', 'cat']
+    $pets = Pet::where('kind', 'dog')
+              ->orWhere('kind', 'cat')
+              ->get();
+
+    return view('view-blade')
+        ->with('tittle', $tittle)
+        ->with('pets', $pets);
+});
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
@@ -29,4 +76,3 @@ Route::middleware(['auth'])->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
